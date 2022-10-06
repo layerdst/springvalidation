78eea71acb2d7d553171a123cd071fc097b9d724
layerdst <bccfcc@hanmail.net>
2022-10-06 22:26:02 +0900

    validation groups

--- main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -2,6 +2,8 @@ package hello.itemservice.web.validation;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -38,41 +40,63 @@ public class ValidationItemControllerV3 {
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
-        return "validation/v2/items";
+        return "validation/v3/items";
     }
 
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "validation/v2/item";
+        return "validation/v3/item";
     }
 
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
-        return "validation/v2/addForm";
+        return "validation/v3/addForm";
     }
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
-        return "validation/v2/editForm";
+        return "validation/v3/editForm";
     }
 
-    //    @PostMapping("/add")
-
-
-    @PostMapping("/add") //Databinder 와 연결이 되게끔 @Validated 추가한다.
+//    @PostMapping("/add") //Databinder 와 연결이 되게끔 @Validated 추가한다.
     public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
-//        Map<String, String> errors = new HashMap<>();
 
-//        itemValidator.validate(item, bindingResult);
+        if(item.getPrice()!= null && item.getQuantity() != null){
+            int resultPrices = item.getPrice() * item.getQuantity();
+            if(resultPrices<10000){
+                bindingResult.addError(new ObjectError("item", null, null,"가격 수량의 합은 1000000입니다"));
+            }
+        }
 
         if (bindingResult.hasErrors()) {
             log.info("errors = {}", bindingResult);
-            return "validation/v2/addForm";
+            return "validation/v3/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+    @PostMapping("/add") //Databinder 와 연결이 되게끔 @Validated 추가한다.
+    public String addItemV7(@Validated(SaveCheck.class) @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        if(item.getPrice()!= null && item.getQuantity() != null){
+            int resultPrices = item.getPrice() * item.getQuantity();
+            if(resultPrices<10000){
+                bindingResult.addError(new ObjectError("item", null, null,"가격 수량의 합은 1000000입니다"));
+            }
+        }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v3/addForm";
         }
 
         Item savedItem = itemRepository.save(item);
@@ -82,8 +106,38 @@ public class ValidationItemControllerV3 {
     }
 
 
+//    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+        if(item.getPrice()!= null && item.getQuantity() != null){
+            int resultPrices = item.getPrice() * item.getQuantity();
+            if(resultPrices<10000){
+                bindingResult.addError(new ObjectError("item", null, null,"가격 수량의 합은 1000000입니다"));
+            }
+        }
+
+        if(bindingResult.hasErrors()){
+            log.info("errors = {}", bindingResult);
+            return "validation/v3/editform";
+        }
+
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    public String edit2(@PathVariable Long itemId, @Validated(UpdateCheck.class) @ModelAttribute Item item, BindingResult bindingResult) {
+        if(item.getPrice()!= null && item.getQuantity() != null){
+            int resultPrices = item.getPrice() * item.getQuantity();
+            if(resultPrices<10000){
+                bindingResult.addError(new ObjectError("item", null, null,"가격 수량의 합은 1000000입니다"));
+            }
+        }
+
+        if(bindingResult.hasErrors()){
+            log.info("errors = {}", bindingResult);
+            return "validation/v3/editform";
+        }
+
         itemRepository.update(itemId, item);
         return "redirect:/validation/v3/items/{itemId}";
     }
